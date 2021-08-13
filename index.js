8d5611aeef11b2265cab7d718384bebf5442aad0
Hashem Gray <Hgray1109@gmail.com>
2021-08-13 17:55:26 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(depth){
+  if (depth <= 400) {
+  return "This one is on me!" 
+  } 
+  if (depth > 2000) {
+    if (depth > 2500 ) {
+      return 'No can do.'
+    }
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return city === 'NYC'? "Ok, sounds good." : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return 'Thank you.'
+    default: 
+      return 'Bye.'
+  }
 }
